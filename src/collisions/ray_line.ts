6063be389d05238b662a2d32e09a53d545f4d64a
kayac <egg734631@gmail.com>
2020-08-06 00:17:03 +0800

    Fix order

--- src/collisions/ray_line.ts
+++ src/collisions/ray_line.ts
@@ -2,7 +2,7 @@ import { Line, Ray } from "./type";
 import { inRange } from "./utils";
 import { sub, det, add } from "../../lib/vec2";
 
-export default function hitTest(o1: Line, o2: Ray) {
+export default function hitTest(o2: Ray, o1: Line) {
   const l1 = sub(o1.start, o1.end);
   const l2 = sub(o2.start, add(o2.start, o2.dir));
   const l3 = sub(o1.start, add(o2.start, o2.dir));
